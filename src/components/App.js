87cb0ac0e7ed1923ac40a145ec9b5b55c403a56d
Sergey Popov <sergeykms@gmail.com>
2021-04-21 15:51:45 +1000

    21_04_2021__2

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 import Main from "./Main";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
-import api from "../utils/Api";
+import api from "../utils/api";
 import {CurrentUserContext} from '../contexts/CurrentUserContext';
 import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
